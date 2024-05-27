9212100bbd74e16ef4ca4dfbfcfe2bf51ae678e4
Petrányi Dominik <h268432@stud.u-szeged.hu>
2024-05-27 18:46:28 +0200

    rolam es header kesz

---
+++ src/app/index/index.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndexRoutingModule } from './index-routing.module';
+import { IndexComponent } from './index.component';
+import { HeaderComponent } from '../header/header.component';
+import { AboutComponent } from '../about/about.component';
+
+
+@NgModule({
+  declarations: [
+    IndexComponent
+  ],
+  imports: [
+    CommonModule,
+    IndexRoutingModule,
+    HeaderComponent,
+    AboutComponent,
+  ]
+})
+export class IndexModule { }
